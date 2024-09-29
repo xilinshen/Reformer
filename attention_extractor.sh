3022b86cd17b23ecacb9b83562cb92fefe416dd9
xilinshen <57839020+xilinshen@users.noreply.github.com>
2024-09-29 19:51:12 +0800

    Update attention_extractor.sh

--- attention_extractor.sh
+++ attention_extractor.sh
@@ -3,4 +3,4 @@ prefix=$1
 python attention_extractor.py \
 --file-path ./data/${prefix}.h5ad \
 --model-path ./model/model.bin \
---save-file ./Data/${prefix}_attention.npz
+--save-file ./data/${prefix}_attention.npz
